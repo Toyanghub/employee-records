6117b1636e5463f6b74b21eded7038e67364c02f
Toyanghub <selwyntayong12@gmail.com>
2025-04-29 15:47:28 +0800

    create the employee model

---
+++ app/Models/Employee.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Employee extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'gender',
+        'birthday',
+        'monthly_salary',
+    ];
+
+    // Optional: If you want to automatically cast the birthday to a Carbon instance
+    protected $dates = ['birthday'];
+
+    // Optional: If you want to cast the monthly_salary to decimal type
+    protected $casts = [
+        'monthly_salary' => 'decimal:2',
+    ];
+}
